30705c8f2c239ef277eecaf4fe1aa33c708c418c
Mauro Garro <maurogarro73@gmail.com>
2022-10-26 19:24:28 -0300

    agregando logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../img/logo.png';
 import CartWidget from './CartWidget';
 
 export default function NavBar() {
@@ -9,7 +10,9 @@ export default function NavBar() {
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Pampa-Ticket</Navbar.Brand>
+          <Navbar.Brand href="#inicio">
+            <img alt="logo" src={logo} width="100%" height="40" className="d-inline-block align-top" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
             <Nav className="">
